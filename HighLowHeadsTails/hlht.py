15353986b598dc3024c9c384d4779fd59151d511
Andrew Plaza <insidious@liquidthink.net>
2015-02-11 12:28:27 -0500

    now i really did start challenge 2

---
+++ HighLowHeadsTails/hlht.py
@@ -0,0 +1,25 @@
+import random
+
+numOfCoins = 0
+
+
+def flipCoin(string):
+    if string == "fifty":
+        randnum
+    elif string == "chance":
+        print("How many coins do you like to flip?")
+        answer = input()
+
+
+def promptUser():
+    print("Are You Playing for a 50/50 flip? (Y/n)")
+    Yn = input()
+    if Yn == "Y":
+        flipCoin("fifty")
+    elif Yn == "n":
+        flipCoin("chance")
+
+promptUser()
+
+
+
