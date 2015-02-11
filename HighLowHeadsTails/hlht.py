eb86cbcd3fb8f91d6aa3d3814e710d9b6d9676cd
Andrew Plaza <andrew.plaza15@gmail.com>
2015-02-11 19:24:12 +0000

    I fixed the failure!

--- HighLowHeadsTails/hlht.py
+++ HighLowHeadsTails/hlht.py
@@ -19,14 +19,21 @@ def flipCoin(string):
 
 
 def promptUser():
+    print("CoinFlip or Higher/Lower?(C/H)")
+    answer = input()
+    if answer == "C":
+        isCoinFlip()
+    elif answer == "H":
+        isHigherLower()
+
+def isCoinFlip(): 
     print("Are You Playing for a 50/50 flip? (Y/n)")
     Yn = input()
     if Yn == "Y":
         flipCoin("fifty")
     elif Yn == "n":
         flipCoin("chance")
-
-promptUser()
-
+def isHigherLower():
+    pass
 
 
