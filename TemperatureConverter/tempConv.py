e3fa16b2dabd109fc34f73d179de5b62fe1ac11c
Andrew Plaza <insidious@liquidthink.net>
2015-02-12 15:59:43 -0500

    started tempConverter

---
+++ TemperatureConverter/tempConv.py
@@ -0,0 +1,18 @@
+
+def convTemp():
+    pass
+
+
+
+print("From What Temperature are you Converting From?((K)elvin,(C)elsius,(F)areinheight")
+unitFrom = input()
+print("What Temperature do you want to Convert To? ((K)elvin,(C)elsius,(F)areinheight)")
+unitTo = input()
+
+def CF(cOrF):
+    pass
+def CK(cOrK):
+    pass
+def FK(fOrK):
+    pass
+def
